5bcf0455951d179d46b26dd0c04a04f63b07079d
josema9119 <jose.ma.mm.91@gmail.com>
2022-05-03 19:45:07 +0000

    no se ve el logo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import SecondCounter from "./secondcounter.js";
 
 const Home = () => {
-	const logo = <i className="fas fa-clock"></i>;
+	const clock = <i class="far fa-clock"></i>;
 	const [timer, setTimer] = useState(0);
 
 	useEffect(() => {
@@ -16,37 +16,37 @@ const Home = () => {
 		<div className="row m-0 vh-100 justify-content-center align-items-center">
 			<div
 				style={{ width: "6rem", height: "7.5rem", borderRadius: 75 }}
-				className="bg-dark text-white text-center m-3 pt-4">
-				<SecondCounter seconds={logo} />
+				className="bg-success text-white text-center m-3 pt-4">
+				<SecondCounter seconds={clock} />
 			</div>
 			<div
 				style={{ width: "6rem", height: "7.5rem", borderRadius: 75 }}
-				className="bg-dark text-white text-center m-3 pt-4">
+				className="bg-success text-white text-center m-3 pt-4">
 				<SecondCounter num={Math.floor((timer / 100000) % 10)} />
 			</div>
 			<div
 				style={{ width: "6rem", height: "7.5rem", borderRadius: 75 }}
-				className="bg-dark text-white text-center m-3 pt-4">
+				className="bg-success text-white text-center m-3 pt-4">
 				<SecondCounter num={Math.floor((timer / 10000) % 10)} />
 			</div>
 			<div
 				style={{ width: "6rem", height: "7.5rem", borderRadius: 75 }}
-				className="bg-dark text-white text-center m-3 pt-4">
+				className="bg-success text-white text-center m-3 pt-4">
 				<SecondCounter num={Math.floor((timer / 1000) % 10)} />
 			</div>
 			<div
 				style={{ width: "6rem", height: "7.5rem", borderRadius: 75 }}
-				className="bg-dark text-white text-center m-3 pt-4">
+				className="bg-success text-white text-center m-3 pt-4">
 				<SecondCounter num={Math.floor((timer / 100) % 10)} />
 			</div>
 			<div
 				style={{ width: "6rem", height: "7.5rem", borderRadius: 75 }}
-				className="bg-dark text-white text-center m-3 pt-4">
+				className="bg-success text-white text-center m-3 pt-4">
 				<SecondCounter num={Math.floor((timer / 10) % 10)} />
 			</div>
 			<div
 				style={{ width: "6rem", height: "7.5rem", borderRadius: 75 }}
-				className="bg-dark text-white text-center m-3 pt-4">
+				className="bg-success text-white text-center m-3 pt-4">
 				<SecondCounter num={Math.floor((timer / 1) % 10)} />
 			</div>
 		</div>
--- src/js/component/secondcounter.js
+++ src/js/component/secondcounter.js
@@ -3,14 +3,10 @@ import PropTypes from "prop-types";
 
 const SecondCounter = (props) => {
 	return (
-		<div className="col">
-			<div className="card bg-light">
-				<div className="card-body d-flex justify-content-center ">
-					<h5 className="card-title ">
-						{props.logo} {props.num}
-					</h5>
-				</div>
-			</div>
+		<div className="card-body d-flex justify-content-center ">
+			<h5 className="card-title ">
+				{props.logo} {props.num}
+			</h5>
 		</div>
 	);
 };
